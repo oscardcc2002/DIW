7b49909a089ff61e479425f74f9e81f2954c8eae
Óscar del Campo Carpio <ratgetdelcampo@gmail.com>
2023-11-15 11:13:56 +0100

    cursoAnimaciones y cursoFormulario

---
+++ cursoAnimaciones/javascript/animacion.js
@@ -0,0 +1,17 @@
+window.onload = function load(){
+
+    let chincheta1Element1 = document.getElementById('chinchi');
+    chincheta1Element1.classList.add('mueveChincheta1');
+  
+    let chincheta1Element2 = document.getElementById('chinchi2');
+    chincheta1Element2.classList.add('mueveChincheta2');
+  
+    let chincheta1Element3 = document.getElementById('chinchi3');
+    chincheta1Element3.classList.add('mueveChincheta3');
+  
+    let articulo1Element = document.getElementsByClassName('trans1');
+    articulo1Element.classList.add('mueveChincheta1');
+  
+    let articulo2Element = document.getElementsByClassName('trans3');
+    articulo2Element.classList.add('mueveChincheta1');
+  }
